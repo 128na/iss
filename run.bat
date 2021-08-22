a9a615437e884c0f0e053cf7bf7cdfa7a03bfd1a
128na <simutrans.128na@gmail.com>
2021-08-22 17:18:30 +0900

    CI test

--- run.bat
+++ run.bat
@@ -1,6 +1,8 @@
+cd src
 :loop
 makeobj pak256
-copy *.pak ..\simutrans_addon_dev\pak128
-..\simutrans_addon_dev\simutrans.exe
+
+copy *.pak ..\..\simutrans_addon_dev\pak128
+..\..\simutrans_addon_dev\simutrans.exe
 
 goto loop
