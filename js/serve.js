06f2745946569447fe6b183e47ff2175951d7da1
128na <simutrans.128na@gmail.com>
2021-08-24 22:28:18 +0900

    ビルドするたびにdefinitionをリロードするように

--- js/serve.js
+++ js/serve.js
@@ -4,6 +4,7 @@ const merge = require('./lib/merge');
 const copyFile = require('./lib/copyFile');
 const makePak = require('./makePak');
 const handleSimutrans = require('./lib/handleSimutrans');
+const reload = require('require-reload')(require);
 
 const targetDir = process.env.TARGET_DIR || './src';
 const outputDir = process.env.OUTPUT_DIR || './dist';
@@ -15,7 +16,7 @@ const simutransPath = process.env.SIMUTRANS_EXECUTABLE;
 const target = ['dat', 'png', 'js'].map(ext => `${targetDir}/*.${ext}`);
 watcher(target, async () => {
   try {
-    const definitions = require('../src/definitions');
+    const definitions = reload('../src/definitions.js');
     const pakFiles = await makePak(makeobjPath, targetDir, outputDir, definitions, pakName);
     const mergePakFile = `${outputDir}/${pakName}`;
 
