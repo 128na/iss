55b0249a1e958902ae89b17182398c856bdcb0b8
128na <simutrans.128na@gmail.com>
2021-08-24 22:27:46 +0900

    タイルのAAで隙間ができるのをいい感じに

--- src/definitions.js
+++ src/definitions.js
@@ -10,12 +10,16 @@ module.exports = [
   {
     size: 256,
     pakFile: 'road.pak',
-    datFiles: ['road.dat'],
+    datFiles: ['rg.dat', 're.dat'],
     imageSet: {
-      'rg_0030.png': [/**'rg_0030_b.png',**/ 'rg_0030_f.png'],
-      'rg_0050.png': [/**'rg_0050_b.png',**/ 'rg_0050_f.png'],
-      'rg_0100.png': [/**'rg_0100_b.png',**/ 'rg_0100_f.png'],
-      'rg_1200.png': [/**'rg_1200_b.png',**/ 'rg_1200_f.png'],
+      'rg_0030.png': ['rg_0030_b.png', 'rg_0030_f.png'],
+      'rg_0050.png': ['rg_0050_b.png', 'rg_0050_f.png'],
+      'rg_0100.png': ['rg_0100_b.png', 'rg_0100_f.png'],
+      'rg_1200.png': ['rg_1200_b.png', 'rg_1200_f.png'],
+      're_0030.png': ['rg_0030_b.png', 're_0030_f.png'],
+      're_0050.png': ['rg_0050_b.png', 're_0050_f.png'],
+      're_0100.png': ['rg_0100_b.png', 're_0100_f.png'],
+      're_1200.png': ['rg_1200_b.png', 're_1200_f.png'],
     }
   }
 ];
