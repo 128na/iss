4c16af6ba2edd24669d8b9c37d6c1c3bd0cb0245
128na <simutrans.128na@gmail.com>
2021-08-28 01:14:27 +0900

    update

--- js/managers/SimutransManager.ts
+++ js/managers/SimutransManager.ts
@@ -7,22 +7,22 @@ export default class SimutransManager {
   private exePath: string;
   private childProcess?: ChildProcessWithoutNullStreams;
 
-  public constructor(pakDir?: string, exePath?: string) {
-    if (!pakDir) {
-      throw new Error('invalid pak directory');
-    }
-    if (!exePath) {
-      throw new Error('invalid exe path');
-    }
+  public constructor(pakDir: string = '', exePath: string = '') {
     this.pakDir = pakDir;
     this.exePath = exePath;
   }
 
   public copyToPakDirectory(from: string, to: string): void {
+    if (!this.pakDir) {
+      throw new Error('invalid pak directory');
+    }
     fs.copySync(from, `${this.pakDir}/${to}`, { overwrite: true });
   }
 
   public reRun() {
+    if (!this.exePath) {
+      throw new Error('invalid exe path');
+    }
     if (this.childProcess) {
       this.childProcess.kill();
     }
