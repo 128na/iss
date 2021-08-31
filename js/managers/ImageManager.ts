508ba272b54a3ab34a0626361e27228e36f2925b
128na <simutrans.128na@gmail.com>
2021-08-31 23:34:08 +0900

    発行色のハンドリングを調整

--- js/managers/ImageManager.ts
+++ js/managers/ImageManager.ts
@@ -1,45 +1,46 @@
 import fs from 'fs-extra';
-import { Canvas, createCanvas, Image } from 'canvas';
+import { Canvas, createCanvas, Image, loadImage } from 'canvas';
 
 export default class ImageManager {
   static SPECIAL_KEYWORD = 'special_color.png';
-  static SPECIAL_COLORS: { [index: string]: [number, number, number] } = {
-    '107,107,107': [108, 108, 108],
-    '155,155,155': [156, 156, 156],
-    '179,179,179': [180, 180, 180],
-    '201,201,201': [202, 202, 202],
-    '223,223,223': [224, 224, 224],
-    '87,101,111': [88, 102, 112],
-    '127,155,241': [128, 156, 242],
-    '255,255,83': [255, 255, 84],
-    '255,33,29': [255, 34, 30],
-    '1,221,1': [2, 222, 2],
-    '227,227,255': [228, 228, 255],
-    '193,177,209': [194, 178, 210],
-    '77,77,77': [78, 78, 78],
-    '255,1,127': [255, 2, 128],
-    '1,1,255': [2, 2, 255],
-    '36,75,103': [37, 76, 104],
-    '57,94,124': [58, 95, 125],
-    '76,113,145': [77, 114, 146],
-    '96,132,167': [97, 133, 168],
-    '116,151,189': [117, 152, 190],
-    '136,171,211': [137, 172, 212],
-    '156,190,233': [157, 191, 234],
-    '176,210,255': [177, 211, 255],
-    '123,88,3': [124, 89, 4],
-    '142,111,4': [143, 112, 5],
-    '161,134,5': [162, 135, 6],
-    '180,157,7': [181, 158, 8],
-    '198,180,8': [199, 181, 9],
-    '217,203,10': [218, 204, 11],
-    '236,226,11': [237, 227, 12],
-    '255,249,13': [255, 250, 14],
-  };
+  static SPECIAL_COLORS: string[] = [
+    '107,107,107',
+    '155,155,155',
+    '179,179,179',
+    '201,201,201',
+    '223,223,223',
+    '87,101,111',
+    '127,155,241',
+    '255,255,83',
+    '255,33,29',
+    '1,221,1',
+    '227,227,255',
+    '193,177,209',
+    '77,77,77',
+    '255,1,127',
+    '1,1,255',
+    '36,75,103',
+    '57,94,124',
+    '76,113,145',
+    '96,132,167',
+    '116,151,189',
+    '136,171,211',
+    '156,190,233',
+    '176,210,255',
+    '123,88,3',
+    '142,111,4',
+    '161,134,5',
+    '180,157,7',
+    '198,180,8',
+    '217,203,10',
+    '236,226,11',
+    '255,249,13',
+  ];
+  static ERASE_COLOR = '255,0,0';
 
   public async merge(output: string, sources: string[]): Promise<void> {
     const hasSpecialImage = sources[sources.length - 1].includes(ImageManager.SPECIAL_KEYWORD);
-    const images = await Promise.all(sources.map(this.createImage));
+    const images = await Promise.all(sources.map(loadImage));
     const canvas = this.mergeImages(images, hasSpecialImage);
 
     this.write(output, canvas.toDataURL());
@@ -50,14 +51,6 @@ export default class ImageManager {
     fs.writeFileSync(output, dataURI.replace(/^data:image\/png;base64,/, ''), 'base64');
   }
 
-  private createImage(filepath: string): Promise<Image> {
-    return new Promise((resolve, reject) => {
-      const img = new Image;
-      img.onload = () => resolve(img);
-      img.onerror = (e) => reject(e);
-      img.src = filepath;
-    });
-  }
   private mergeImages(images: Image[], hasSpecialImage = false): Canvas {
     const special = hasSpecialImage ? images.pop() : undefined;
 
@@ -82,8 +75,15 @@ export default class ImageManager {
     for (let i = 0; i < data.length; i += 4) {
       const color = [data[i], data[i + 1], data[i + 2]].join(',');
 
-      if (ImageManager.SPECIAL_COLORS[color]) {
-        [data[i], data[i + 1], data[i + 2]] = ImageManager.SPECIAL_COLORS[color];
+      if (ImageManager.SPECIAL_COLORS.includes(color)) {
+        [data[i], data[i + 1], data[i + 2]] = [
+          Math.min(255, data[i] + 1),
+          Math.min(255, data[i + 1] + 1),
+          Math.min(255, data[i + 2] + 1),
+        ];
+      }
+      if (color === ImageManager.ERASE_COLOR) {
+        data[i + 3] = 0;
       }
     }
     ctx.putImageData(imageData, 0, 0);
