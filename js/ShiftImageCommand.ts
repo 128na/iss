53ab5f49181d8684d0313051e80fa0d893e07dfa
128na <simutrans.128na@gmail.com>
2021-09-05 21:49:12 +0900

    ライブラリ分離

--- js/ShiftImageCommand.ts
+++ js/ShiftImageCommand.ts
@@ -1,6 +1,5 @@
-import fs from 'fs-extra';
 import { Command } from 'commander';
-import { createCanvas, Image, loadImage } from 'canvas';
+import ImageManager from './managers/ImageManager';
 
 /**
  * npx ts-node .\js\ShiftImageCommand.ts --source=.\src\path\to\file.png
@@ -11,40 +10,14 @@ runner
   .option('-i, --inputSize <pixcel>', 'Split size', '128')
   .option('-o, --outputSize <pixcel>', 'Split size', '256')
   .option('-s, --source <directory>', 'Source file path')
-  .action((options: { inputSize: string, outputSize: number, source: string }) => {
+  .action(async (options: { inputSize: string, outputSize: number, source: string }) => {
     const inputSize = Number(options.inputSize);
     const outputSize = Number(options.outputSize);
     const source = options.source;
-    console.log({ inputSize, outputSize, source });
-    loadImage(source)
-      .then(image =>
-        shiftImage(image, inputSize, outputSize, source.replace('.png', '_shift.png')))
-      .catch(e => console.log({ e }))
+
+    const imageManager = new ImageManager();
+
+    await imageManager.shiftImage(source, inputSize, outputSize, source.replace('.png', '_shift.png'));
   })
   .showHelpAfterError()
   .parse();
-
-function shiftImage(image: Image, is: number = 128, os: number = 256, output: string): void {
-  const n = (image.width - is * 2) / is;
-  if (!Number.isInteger(n)) {
-    throw new Error("invalid size:" + n);
-  }
-
-  const w = os * (n + 1) * 2;
-  const h = image.height;
-  const canvas = createCanvas(w, h);
-  const ctx = canvas.getContext('2d');
-
-  for (let index = 0; index <= n; index++) {
-    const [sx, sy, sw, sh] = [index * is / 2, index * is / 4, is, h - index * is / 4];
-    const [dx, dy] = [index * os, 0];
-    ctx.drawImage(image, sx, sy, sw, sh, dx, dy, sw, sh);
-  }
-  for (let index = 0; index <= n; index++) {
-    const [sx, sy, sw, sh] = [image.width / 2 + index * is / 2, (n - index) * is / 4, is, h - index * is / 4];
-    const [dx, dy] = [w / 2 + index * os, 0];
-    ctx.drawImage(image, sx, sy, sw, sh, dx, dy, sw, sh);
-  }
-  fs.ensureFileSync(output);
-  fs.writeFileSync(output, canvas.toDataURL().replace(/^data:image\/png;base64,/, ''), 'base64');
-}
--- js/SplitImageCommand.ts
+++ js/SplitImageCommand.ts
@@ -1,6 +1,5 @@
-import fs from 'fs-extra';
 import { Command } from 'commander';
-import { createCanvas, Image, loadImage } from 'canvas';
+import ImageManager from './managers/ImageManager';
 
 /**
  * npx ts-node .\js\SplitImageCommand.ts --source=.\src\path\to\file.png
@@ -10,29 +9,13 @@ runner
   .description('画像分割')
   .option('-i, --inputSize <pixcel>', 'Split size', '128')
   .option('-s, --source <directory>', 'Source file path')
-  .action((options: { inputSize: string, source: string }) => {
+  .action(async (options: { inputSize: string, source: string }) => {
     const inputSize = Number(options.inputSize);
     const source = options.source;
-    console.log({ inputSize, source });
-    loadImage(source)
-      .then(image => splitImage(image, inputSize, source))
-      .catch(e => console.log({ e }))
+
+    const imageManager = new ImageManager();
+
+    await imageManager.splitImage(source, inputSize);
   })
   .showHelpAfterError()
   .parse();
-
-function splitImage(image: Image, size: number = 128, output: string): void {
-  const [width, height] = [image.width, image.height];
-
-  for (let x = 0; x < width; x += size) {
-    for (let y = 0; y < height; y += size) {
-      const canvas = createCanvas(size, size);
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(image, -x, -y);
-
-      const filepath = output.replace('.png', `_${y}.${x}.png`);
-      fs.ensureFileSync(filepath);
-      fs.writeFileSync(filepath, canvas.toDataURL().replace(/^data:image\/png;base64,/, ''), 'base64');
-    }
-  }
-}
