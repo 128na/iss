6a22887791befc2dfdfd32f83afeb29e6e06da24
128na <simutrans.128na@gmail.com>
2021-09-20 17:33:45 +0900

    画像マージを高速化

--- js/managers/ImageManager.ts
+++ js/managers/ImageManager.ts
@@ -1,6 +1,6 @@
 import fs from 'fs-extra';
-import { Canvas, createCanvas, Image, loadImage } from 'canvas';
-import { eraseColor, mergeImage, replaceSpecialColor, shiftImage, splitImage } from 'simutrans-image-util';
+import { Image, loadImage } from 'canvas';
+import { mergeImage, shiftImage, splitImage } from 'simutrans-image-util';
 
 export default class ImageManager {
   static SPECIAL_KEYWORD = 'special_color.png';
@@ -8,27 +8,36 @@ export default class ImageManager {
   static ERASE_COLOR = '255,0,0';
 
   public async merge(output: string, sources: string[]): Promise<void> {
+    let images: Image[] = [];
     let canvas;
-    let ctx;
 
     for (const source of sources) {
-      const image = await loadImage(source);
-      if (!canvas || !ctx) {
-        canvas = createCanvas(image.width, image.height);
-        ctx = canvas.getContext('2d');
-      }
-      ctx.drawImage(image, 0, 0);
+      images.push(await loadImage(source));
+      const special = source.endsWith(ImageManager.SPECIAL_KEYWORD);
+      const erase = source.endsWith(ImageManager.ERASE_KEYWORD);
 
-      if (!source.endsWith(ImageManager.SPECIAL_KEYWORD)) {
-        replaceSpecialColor(canvas);
-      }
-      if (source.endsWith(ImageManager.ERASE_KEYWORD)) {
-        eraseColor(canvas, ImageManager.ERASE_COLOR);
+      if (special || erase) {
+        canvas = mergeImage(images, {
+          canvas,
+          replaceSpecialColor: special,
+          eraseColor: erase ? ImageManager.ERASE_COLOR : undefined,
+        });
+        images = [];
       }
     }
+
+    if (images.length) {
+      canvas = mergeImage(images, {
+        canvas,
+        replaceSpecialColor: true,
+        eraseColor: ImageManager.ERASE_COLOR,
+        eraseTransparent: false
+      });
+    }
     if (canvas) {
-      // eraseTransparent(canvas, 0.5);
       this.write(output, canvas.toDataURL());
+    } else {
+      throw new Error("missing canvas");
     }
   }
 
@@ -54,20 +63,4 @@ export default class ImageManager {
       fs.writeFileSync(filepath, canvas.toDataURL().replace(/^data:image\/png;base64,/, ''), 'base64');
     });
   }
-}
-
-function eraseTransparent(canvas: Canvas, threshold = 0.5): void {
-  const ctx = canvas.getContext('2d');
-  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  const data = imageData.data;
-  const t = 255 * threshold;
-  for (let i = 0; i < data.length; i += 4) {
-    if (data[i + 3] < t) {
-      data[i + 0] = 231;
-      data[i + 1] = 255;
-      data[i + 2] = 255;
-    }
-    data[i + 3] = 255;
-  }
-  ctx.putImageData(imageData, 0, 0);
 }
