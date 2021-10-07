24b7ac9490d7b8d28f8e3d65696743a7e4c35dab
128na <simutrans.128na@gmail.com>
2021-10-07 09:13:06 +0900

    Revert "側壁調整"
    This reverts commit c754ba5948c54c54c33631e93dcec27a45df03fb.

--- js/libs/ImageManager.ts
+++ js/libs/ImageManager.ts
@@ -8,25 +8,22 @@ export default class ImageManager {
   private eraseColor: string;
   private keepTransparentKeyword: string;
   private eraseTransparentThreshold: number;
-  private edgeOnlyKeyword: string;
 
   constructor(
     specialKeyword = '_sc.png',
     eraseKeyword = '_ec.png',
     eraseColor = '255,0,0',
     keepTransparentKeyword = '_kt.png',
-    eraseTransparentThreshold: string | number = 0.1,
-    edgeOnlyKeyword = '_ed.png',
+    eraseTransparentThreshold: string | number = 0.1
   ) {
     this.specialKeyword = specialKeyword;
     this.eraseKeyword = eraseKeyword;
     this.eraseColor = eraseColor;
     this.keepTransparentKeyword = keepTransparentKeyword;
     this.eraseTransparentThreshold = Number(eraseTransparentThreshold);
-    this.edgeOnlyKeyword = edgeOnlyKeyword;
   }
 
-  public async merge(output: string, sources: string[]) {
+  public async merge(output: string, sources: string[]): Promise<void> {
     let images: Image[] = [];
     let canvas;
 
@@ -54,14 +51,9 @@ export default class ImageManager {
       });
     }
     if (canvas) {
-      if (output.endsWith(this.keepTransparentKeyword)) {
-        return this.write(output, canvas.toDataURL());
+      if (!output.endsWith(this.keepTransparentKeyword)) {
+        eraseTransparent(canvas, this.eraseTransparentThreshold);
       }
-      if (output.endsWith(this.edgeOnlyKeyword)) {
-        eraseTransparent(canvas, this.eraseTransparentThreshold, true);
-        return this.write(output, canvas.toDataURL());
-      }
-      eraseTransparent(canvas, this.eraseTransparentThreshold);
       this.write(output, canvas.toDataURL());
     } else {
       throw new Error("missing canvas");
