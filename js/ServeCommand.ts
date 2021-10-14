12662c88e6df3213f7ad5936ae6e778a0631d8ee
128na <simutrans.128na@gmail.com>
2021-10-14 17:10:41 +0900

    definitionsをjsonに統一

--- js/ServeCommand.ts
+++ js/ServeCommand.ts
@@ -7,7 +7,7 @@ require('dotenv').config();
 const runner = new Command('build');
 runner
   .description('ソースファイルの更新を監視して自動ビルド、検証用シムトラを起動します。')
-  .option('-d, --definition <file>', 'Definition file path', './src/definitions.js')
+  .option('-d, --definition <file>', 'Definition file path', './src/definitions.json')
   .option('-s, --source <directory>', 'Source directory path', './src')
   .option('-o, --output <directory>', 'Output directory path', './dist')
   .option('-p, --paklib <file>', 'Output pak file library path', 'dev.pak')
