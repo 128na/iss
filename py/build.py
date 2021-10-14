3321b104f157f118ffa8bb3c0864ada97c1b6c0f
128na <simutrans.128na@gmail.com>
2021-10-14 22:12:36 +0900

    マルチスレッドリターンズ

--- py/build.py
+++ py/build.py
@@ -8,48 +8,59 @@ from libs import makeobjManager
 from multiprocessing import Process
 
 
-def handleDefinitions(definition: list[dict], inputPath, outputPath, makeobjpath):
+def handleDefinitions(definitions: list[dict], inputPath, outputPath, makeobjpath, multithread):
     for definition in definitions:
-        handleDefinition(definition, inputPath, outputPath, makeobjpath)
+        if(multithread):
+            handleImagesMulti(definition, inputPath, outputPath)
+        else:
+            handleImages(definition, inputPath, outputPath)
+
+        for datFile in definition['datFiles']:
+            fileManager.copyFile(inputPath+'/'+datFile, outputPath+'/'+datFile)
+
+        makeobjManager.pak(
+            makeobjpath,
+            outputPath,
+            definition['size'],
+            definition['pakFile'],
+            definition['datFiles']
+        )
 
 
-def handleDefinition(definition: dict, inputPath, outputPath, makeobjpath):
-    # process_list = []
+def handleImagesMulti(definition: dict, inputPath, outputPath):
+    print('handleImagesMulti')
+    process_list = []
     for output, inputs in definition['imageSet'].items():
         print('mergeImage', output, inputs)
         if(fileManager.existsFile(outputPath+'/'+output)):
             print('skip')
             continue
-        # if(os.getenv('MULTITHREAD')):
-        #     process = Process(
-        #         target=imageManager.mergeImage,
-        #         args=(inputs, output,  inputPath, outputPath)
-        #     )
-        #     process.start()
-        #     process_list.append(process)
-        # else:
+        process = Process(
+            target=imageManager.mergeImage,
+            args=(inputs, output,  inputPath, outputPath)
+        )
+        process.start()
+        process_list.append(process)
+
+    for process in process_list:
+        process.join()
+
+
+def handleImages(definition: dict, inputPath, outputPath):
+    print('handleImages')
+    for output, inputs in definition['imageSet'].items():
+        print('mergeImage', output, inputs)
+        if(fileManager.existsFile(outputPath+'/'+output)):
+            print('skip')
+            continue
         imageManager.mergeImage(inputs, output,  inputPath, outputPath)
-
-    # if(os.getenv('MULTITHREAD')):
-    #     for process in process_list:
-    #         process.join()
-
-    for datFile in definition['datFiles']:
-        fileManager.copyFile(inputPath+'/'+datFile, outputPath+'/'+datFile)
-
-    makeobjManager.pak(
-        makeobjpath,
-        outputPath,
-        definition['size'],
-        definition['pakFile'],
-        definition['datFiles']
-    )
 
 
 if __name__ == '__main__':
     load_dotenv()
     makeobjpath = os.getenv('MAKEOBJ_PATH')
-    print('MULTITHREAD', os.getenv('MULTITHREAD'))
+    multithread = os.getenv(
+        'MULTITHREAD', 'False').lower() in ('true', '1', 't')
 
     definitionsPath = sys.argv[1]
     outputPath = sys.argv[2]
@@ -57,4 +68,5 @@ if __name__ == '__main__':
     definitions = fileManager.loadJson(definitionsPath)
 
     fileManager.removeDir(outputPath)
-    handleDefinitions(definitions, inputPath, outputPath, makeobjpath)
+    handleDefinitions(definitions, inputPath, outputPath,
+                      makeobjpath, multithread)
