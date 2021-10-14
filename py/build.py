618490a21a790409636e07e5b59de802aa3bb6ba
128na <simutrans.128na@gmail.com>
2021-10-14 20:47:15 +0900

    並列やめ

--- py/build.py
+++ py/build.py
@@ -14,25 +14,25 @@ def handleDefinitions(definition: list[dict], inputPath, outputPath, makeobjpath
 
 
 def handleDefinition(definition: dict, inputPath, outputPath, makeobjpath):
-    process_list = []
+    # process_list = []
     for output, inputs in definition['imageSet'].items():
         print('mergeImage', output, inputs)
         if(fileManager.existsFile(outputPath+'/'+output)):
             print('skip')
             continue
-        if(os.getenv('MULTITHREAD')):
-            process = Process(
-                target=imageManager.mergeImage,
-                args=(inputs, output,  inputPath, outputPath)
-            )
-            process.start()
-            process_list.append(process)
-        else:
-            imageManager.mergeImage(inputs, output,  inputPath, outputPath)
+        # if(os.getenv('MULTITHREAD')):
+        #     process = Process(
+        #         target=imageManager.mergeImage,
+        #         args=(inputs, output,  inputPath, outputPath)
+        #     )
+        #     process.start()
+        #     process_list.append(process)
+        # else:
+        imageManager.mergeImage(inputs, output,  inputPath, outputPath)
 
-    if(os.getenv('MULTITHREAD')):
-        for process in process_list:
-            process.join()
+    # if(os.getenv('MULTITHREAD')):
+    #     for process in process_list:
+    #         process.join()
 
     for datFile in definition['datFiles']:
         fileManager.copyFile(inputPath+'/'+datFile, outputPath+'/'+datFile)
