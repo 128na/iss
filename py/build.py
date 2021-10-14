e5ebe119e2e7dade85d3d9c12b5f74ac2f5f5f04
128na <simutrans.128na@gmail.com>
2021-10-14 17:52:08 +0900

    並列テスト

--- py/build.py
+++ py/build.py
@@ -5,6 +5,7 @@ from libs import fileManager
 from libs import imageManager
 
 from libs import makeobjManager
+from multiprocessing import Process
 
 
 def handleDefinitions(definition: list[dict], inputPath, outputPath, makeobjpath):
@@ -13,12 +14,22 @@ def handleDefinitions(definition: list[dict], inputPath, outputPath, makeobjpath
 
 
 def handleDefinition(definition: dict, inputPath, outputPath, makeobjpath):
+    process_list = []
     for output, inputs in definition['imageSet'].items():
         print('mergeImage', output, inputs)
         if(fileManager.existsFile(outputPath+'/'+output)):
             print('skip')
             continue
-        imageManager.mergeImage(inputs, output,  inputPath, outputPath)
+        # imageManager.mergeImage(inputs, output,  inputPath, outputPath)
+        process = Process(
+            target=imageManager.mergeImage,
+            args=(inputs, output,  inputPath, outputPath)
+        )
+        process.start()
+        process_list.append(process)
+
+    for process in process_list:
+        process.join()
 
     datFiles = []
     for datFile in definition['datFiles']:
@@ -42,4 +53,5 @@ inputPath = './src'
 definitions = fileManager.loadJson(definitionsPath)
 
 fileManager.removeDir(outputPath)
-handleDefinitions(definitions, inputPath, outputPath, makeobjpath)
+if __name__ == '__main__':
+    handleDefinitions(definitions, inputPath, outputPath, makeobjpath)
