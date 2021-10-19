298d2004381cab6d1ff73a53b3312ca9af883d55
128na <simutrans.128na@gmail.com>
2021-10-19 23:41:34 +0900

    format

--- py/libs/makeobjManager.py
+++ py/libs/makeobjManager.py
@@ -3,8 +3,7 @@ import subprocess
 
 def pak(makeobjpath: str, outputDir: str, size: int, pakFile: str, datFiles: list[str]):
 
-    command = makeobjpath+' pak' + \
-        str(size) + ' ' + pakFile+' ' + (' '.join(datFiles))
+    command = f"{makeobjpath} pak{str(size)} {pakFile} {' '.join(datFiles)}"
 
     print(command)
     res = subprocess.run(command, cwd=outputDir, shell=True,
